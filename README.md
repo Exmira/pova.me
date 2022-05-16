1bbc4fc4924a12afb8c4eb5f82edd0859ffb5185
Pova <Malasgharian@gmail.com>
2022-05-16 11:18:02 +0100

    fixed contact feature, smtpjs does not allow gmail smtp anymore so went with Postmailinvotes

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 The official website for https://pova.me
 
 Written in HTML CSS and JavaScript by Pova
-AD
--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["experiences.", "top tier websites.", "opportunities."],
+        strings: ["top tier websites.", "experiences", "opportunities."],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
